bfc725491e838cfe7c613af2668e2936ff7635b5
danieluche <dank.uche@yahoo.com>
2020-05-03 23:35:31 +0100

    update auth strategy

--- account-service/app.js
+++ account-service/app.js
@@ -6,6 +6,7 @@ var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
+app.use('/auth', authRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
